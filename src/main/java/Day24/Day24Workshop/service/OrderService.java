1dc37964ee1e0aebc748f9672da0a3dd67cbab50
boris <BORI0001@e.ntu.edu.sg>
2024-01-24 01:50:19 +0800

    first commit

--- src/main/java/Day24/Day24Workshop/service/OrderService.java
+++ src/main/java/Day24/Day24Workshop/service/OrderService.java
@@ -1,8 +1,5 @@
 package Day24.Day24Workshop.service;
 
-import java.util.Random;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
